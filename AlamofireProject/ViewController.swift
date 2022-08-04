98b327b6395a3723e0a2a34e1cd5cfa3ecd71224
Fedor Donskov <fedya_donskov@mail.ru>
2022-08-04 13:31:48 +0300

    Initial Commit

---
+++ AlamofireProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlamofireProject
+//
+//  Created by Федор Донсков on 04.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
