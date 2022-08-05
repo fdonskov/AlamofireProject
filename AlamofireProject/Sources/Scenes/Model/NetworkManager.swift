281e1682dc6827e887ed62420e2e933b1b578e49
Fedor Donskov <fedya_donskov@mail.ru>
2022-08-05 13:58:01 +0300

    Added and create NetworkManager

--- AlamofireProject/Sources/Scenes/Model/NetworkManager.swift
+++ AlamofireProject/Sources/Scenes/Model/NetworkManager.swift
@@ -6,7 +6,25 @@
 //
 
 import Foundation
+import Alamofire
+
+protocol NetworkManagerDelegate {
+    func updateUI(for model: [MarvelResults])
+}
 
 class NetworkManager {
+    var delefate: NetworkManagerDelegate?
     
+    func networkRequest() {
+        AF.request("https://gateway.marvel.com:443/v1/public/characters?orderBy=name&limit=1&apikey=180f88a51ebc9bbabf73c54e5deb800a")
+            .validate()
+            .responseDecodable(of: MarvelCharacterModel.self) { (data) in
+                guard let responce = data.value else { return }
+                let characters = responce.data.results
+                
+                DispatchQueue.main.async {
+                    self.delefate?.updateUI(for: characters)
+                }
+            }
+    }
 }
